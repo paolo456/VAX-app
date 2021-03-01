8109d7b0e09a04cb53682c35e09c1dbc4cb84b5e
paolo bondi <paolo456@gmail.com>
2021-03-01 12:55:24 -0800

    config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	consumer_key:         '1POS8L7NxG4Ej6nmkxBYxkSZD',
+	consumer_secret:      'M7E9ElssgE96LhGqaUl9TSH86ikFxXmLEZyyEcAKkT06H7JegC',
+	access_token:         '1365446558892761093-QawefkuCkA5fsZyIBbNPmLeucFuAse',
+	access_token_secret:  'KHH84s0pZ3yW0LAoNd74fT0qycvAvq1YF7f5P7CeiRFu8',
+	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+	strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
