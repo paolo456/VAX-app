fd9f4644cc6058eaedfe45b9e0b4d4bd4c57955f
paolo bondi <paolo456@gmail.com>
2021-03-03 16:12:12 -0800

    Routing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 let app = express()
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 app.get('/', function (req, res) {
-	res.render('index', {});
+	res.send('Connected!');
   })
 
 
