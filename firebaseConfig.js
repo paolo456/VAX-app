800c2a39a02fff41e2201a7b0dd93e696f803f50
paolo bondi <paolo456@gmail.com>
2021-04-11 20:47:58 -0700

    New firebase server

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
+	/*
     apiKey: "AIzaSyDnSlmhfPtMkAPkC5b_8OVxGB1EvwLwJBw",
     authDomain: "vaxdb-ac3f3.firebaseapp.com",
     projectId: "vaxdb-ac3f3",
@@ -6,4 +7,16 @@ module.exports = {
     messagingSenderId: "169100254110",
     appId: "1:169100254110:web:7271ba3d3ce888b9c5548f",
     measurementId: "G-BZVP2H4XHM"
+  };
+    // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+	  */
+    apiKey: "AIzaSyDsv1MJ1uapDTf4cY0xlgV0t2afaPJTMWk",
+    authDomain: "vax-app.firebaseapp.com",
+    projectId: "vax-app",
+    storageBucket: "vax-app.appspot.com",
+    messagingSenderId: "75150860638",
+    appId: "1:75150860638:web:d0d6a9e0ac099bd4602fcf",
+    measurementId: "G-1YFZNLR8NN"
   };
